fbbb53979e997245f3abdef230bffb5333cf81dc
Michel Fortin <michel.fortin@michelf.com>
2025-02-02 22:37:53 +0900

    Forgot `public` on verticalError function.

--- Sources/VerticalError/VerticalError.swift
+++ Sources/VerticalError/VerticalError.swift
@@ -1,6 +1,6 @@
 import VerticalErrorC
 
-func verticalError(_ message: String = "", file: StaticString = #fileID, line: UInt = #line) {
+public func verticalError(_ message: String = "", file: StaticString = #fileID, line: UInt = #line) {
 	#if os(macOS) || targetEnvironment(macCatalyst) || os(iOS) || os(tvOS) || os(watchOS) || os(visionOS)
 	_VerticalError_BOTTOM(message, file.utf8Start, Int32(line)) { _ in
 		fatalError(message, file: file, line: line)
