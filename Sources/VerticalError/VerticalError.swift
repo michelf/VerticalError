63e5fae85fa3d7b25086ed67193d9539ca658f25
Michel Fortin <michel.fortin@michelf.com>
2025-02-02 23:45:21 +0900

    Made verticalError a function that never returns in Swift.

--- Sources/VerticalError/VerticalError.swift
+++ Sources/VerticalError/VerticalError.swift
@@ -1,12 +1,12 @@
 import VerticalErrorC
 
-public func verticalError(_ message: String = "", file: StaticString = #fileID, line: UInt = #line) {
+public func verticalError(_ message: String = "", file: StaticString = #fileID, line: UInt = #line) -> Never {
 	#if os(macOS) || targetEnvironment(macCatalyst) || os(iOS) || os(tvOS) || os(watchOS) || os(visionOS)
 	_VerticalError_BOTTOM(message, file.utf8Start, Int32(line)) { _ in
 		fatalError(message, file: file, line: line)
 	}
-	#else
-	// vertical errors not supported, downgrading to standard fatal error.
-	fatalError(message, file: file, line: line)
 	#endif
+	// vertical errors not supported on this platform or closure aboce was not
+	// called for some reason so handle here as a last resort.
+	fatalError(message, file: file, line: line)
 }
